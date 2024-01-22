99272e2587b3a071b67fa22c607eb0f610a57928
mike_varona <migueluk91@gmail.com>
2024-01-22 20:39:54 +0000

    Interacting with Dropdown

--- tests/example.test.js
+++ tests/example.test.js
@@ -11,6 +11,10 @@ describe("My First Puppeteer Test", () => {
     await page.goto("https://devexpress.github.io/testcafe/example");
     await page.type("#developer-name", "Mike", { delay: 0 }); // delay 0ms
     await page.click("#tried-test-cafe", { clickCount: 1 }); // click once
+    await page.select("#preferred-interface", "JavaScript API"); // select by value attribute value of JavaScript API
+    await page.type("#comments", "Here is some comment", { delay: 0 }); 
+    await page.click("#submit-button");
+    await page.waitForSelector(".result-content"); // wait for selector to load on the page before closing the browser
     await page.waitForTimeout(3000);
     await browser.close();
   });
