b6813abe3b23a230d88f79072f6a9d4f993797aa
mike_varona <migueluk91@gmail.com>
2024-01-22 20:33:25 +0000

    Interacting with Buttons & Checkbox

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,12 +4,13 @@ describe("My First Puppeteer Test", () => {
   it("should launch the browser", async () => {
     const browser = await puppeteer.launch({
       headless: false,
-        slowMo: 50, // slow down by 50ms
-        devtools: false // it will not open dev tools
+      slowMo: 50, // slow down by 50ms
+      devtools: false, // it will not open dev tools
     });
     const page = await browser.newPage();
     await page.goto("https://devexpress.github.io/testcafe/example");
     await page.type("#developer-name", "Mike", { delay: 0 }); // delay 0ms
+    await page.click("#tried-test-cafe", { clickCount: 1 }); // click once
     await page.waitForTimeout(3000);
     await browser.close();
   });
