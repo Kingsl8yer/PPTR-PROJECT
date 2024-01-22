501d97bce6e2a416d57afdc31c885c5668c4b834
mike_varona <migueluk91@gmail.com>
2024-01-22 23:15:16 +0000

    Get Element Count

--- tests/example.test.js
+++ tests/example.test.js
@@ -12,7 +12,9 @@ describe("My First Puppeteer Test", () => {
     const title = await page.title(); // get title of the page
     const url = await page.url(); // get url of the page
     const text = await page.$eval("h1", (element) => element.textContent); // get text of the page by using selector h1
+    const count = await page.$$eval("p", (element) => element.length); // get count of the p tags
     console.log("Text in the H1: " + text);
+    console.log("Number of P tags on the page: " + count);
     await browser.close();
   });
 });
