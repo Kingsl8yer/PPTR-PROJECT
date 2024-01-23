fa55e967bfa3befc79262d70afcf12e3bbaaad2a
mike_varona <migueluk91@gmail.com>
2024-01-23 09:51:45 +0000

    Assertions

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,9 +1,10 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
+import { expect } from "chai";
 
 describe("My First Puppeteer Test", () => {
   it("should launch the browser", async () => {
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       slowMo: 50, // slow down by 50ms
       devtools: false, // it will not open dev tools
     });
@@ -13,8 +14,10 @@ describe("My First Puppeteer Test", () => {
     const url = await page.url(); // get url of the page
     const text = await page.$eval("h1", (element) => element.textContent); // get text of the page by using selector h1
     const count = await page.$$eval("p", (element) => element.length); // get count of the p tags
-    console.log("Text in the H1: " + text);
-    console.log("Number of P tags on the page: " + count);
+    expect(title).to.be.a("string", "Example Domain");
+    expect(url).to.include("example.com");
+    expect(text).to.be.a("string", "Example Domain");
+    expect(count).to.equal(2);
     await browser.close();
   });
 });
